9cbd41860602d62ae1d0dab8ddc9c6b0630fa7ab
kimyohan <rladygks1210@naver.com>
2025-03-27 11:38:56 +0900

    "[feat] catch 수정하여 에러메세지 응답"

--- app/Http/Controllers/CalculatorController.php
+++ app/Http/Controllers/CalculatorController.php
@@ -11,37 +11,30 @@ class CalculatorController extends Controller
     {
         // JSON 요청에서 값 받기
         $expression = $request->input('expressions');
-        $result = null;
-        $isValid = true;
-        $message = '';
-
-
-        // try, catch로 에러 발생 점검
         try {
-            // 수식계산시도
             $result = eval('return ' . trim($expression) . ';');
-            $message = "계산 결과는 {$result}입니다!";
-        } catch (\Throwable $e) {
-            // 에러 발생 시
-            $isValid = false;
-            $message = "잘못된 수식입니다!";
-        }
 
-        // db에 저장
-        Calculation::create([
-            'expressions' => $expression,
-            'result' => $result ?? 0,
-            'is_valid' => $isValid,
-        ]);
-
-        // 응답 반환
-        return response()->json([
-            "message" => $message,
-            "data" => [
+            // DB 저장
+            Calculation::create([
                 'expressions' => $expression,
-                'result' => $result ?? null,
-                'is_valid' => $isValid
-            ]
-        ]);
+                'result' => $result,
+                'is_valid' => true,
+            ]);
+
+            return response()->json([
+                'message' => "계산 결과는 {$result}입니다!",
+                'data' => [
+                    'expressions' => $expression,
+                    'result' => $result,
+                    'is_valid' => true
+                ]
+            ]);
+        } catch (\Throwable $e) {
+            // 그냥 에러 나면 여기서 끝!
+            return response()->json([
+                'message' => '잘못된 수식입니다!',
+                'error' => $e->getMessage() // 필요하면 표시
+            ], 403);
+        }
     }
 }
